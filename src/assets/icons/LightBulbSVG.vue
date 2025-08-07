6cf17ea506536733b62646285401d8cde31c3a8e
Renan Martins <renanmartins989@gmail.com>
2025-08-07 04:49:42 +0100

    home first cards done

---
+++ src/assets/icons/LightBulbSVG.vue
@@ -0,0 +1,25 @@
+<template>
+  <svg :width="width" :height="height" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" :stroke="stroke" :stroke-width="strokeWidth" stroke-linecap="round" stroke-linejoin="round"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <line x1="9" y1="18" x2="15" y2="18"></line> <line x1="10" y1="22" x2="14" y2="22"></line> <path d="M15.09 14c.18-.98.65-1.74 1.41-2.5A4.65 4.65 0 0018 8 6 6 0 006 8c0 1 .23 2.23 1.5 3.5A4.61 4.61 0 018.91 14"></path> </g></svg>
+</template>
+
+<script setup>
+
+defineProps({
+  width: {
+    type: String,
+    default: '16'
+  },
+  height: {
+    type: String,
+    default: '16'
+  },
+  stroke: {
+    type: String,
+    default: '#000000'
+  },
+  strokeWidth: {
+    type: Number,
+    default: 1
+  }
+})
+</script>
--- src/components/HomeWhyCreate.vue
+++ src/components/HomeWhyCreate.vue
@@ -1,27 +1,44 @@
 <template>
   <div class="home-why-create">
-    <h1>Why Create Time Capsules?</h1>
-    <p>Discover the powerful benefits of connecting with your future self through meaningful messages</p>
+
+    <div class="home-why-create-header">
+      <h1>Why Create Time Capsules?</h1>
+      <p>Discover the powerful benefits of connecting with your future self through meaningful messages</p>
+    </div>
+
 
     <div class="home-why-create-cards">
-      <div class="home-why-create-card">
-        <h2>Personalized Memories</h2>
-        <p>Share your thoughts, dreams, and memories with your future self, creating a unique and meaningful experience.</p>
+      <div class="home-why-create-card blue-bg">
+        <div class="home-why-create-card-header">
+          <HeartSVG :width="'48'" :height="'48'" class="home-why-card-icon" />
+          <h2>Emotional Connection</h2>
+        </div>
+        <p>Create a meaningful dialogue with your future self, capturing your current thoughts, dreams, and feelings in a moment of time.</p>
       </div>
-      <div class="home-why-create-card">
-        <h2>Future Proofing</h2>
-        <p>Time capsules provide a secure and long-lasting way to preserve memories and dreams for generations to come.</p>
+
+      <div class="home-why-create-card green-bg">
+        <div class="home-why-create-card-header">
+          <LightBulbSVG :width="'48'" :height="'48'" :stroke="'#17A34A'" :strokeWidth="1.7" class="home-why-card-icon" />
+          <h2>Personal Growth</h2>
+        </div>
+        <p>Reflect on your journey, track your progress, and gain insights into how you've evolved and grown over time.</p>
       </div>
-      <div class="home-why-create-card">
-        <h2>Personalized Memories</h2>
-        <p>Share your thoughts, dreams, and memories with your future self, creating a unique and meaningful experience.</p>
+
+      <div class="home-why-create-card purple-bg">
+        <div class="home-why-create-card-header">
+          <TargetSVG :width="'48'" :height="'48'" :stroke="'#9334E9'" :strokeWidth="1.7" class="home-why-card-icon" />
+          <h2>Goal Achievement</h2>
+        </div>
+        <p>Set intentions and goals, then rediscover them later to see how far you've come and what you've accomplished.</p>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-
+import HeartSVG from '@/assets/icons/HeartSVG.vue';
+import LightBulbSVG from '@/assets/icons/LightBulbSVG.vue';
+import TargetSVG from '@/assets/icons/TargetSVG.vue';
 </script>
 
 <style scoped>
@@ -32,13 +49,21 @@
   justify-content: center;
   width: 100%;
   height: 100%;
-  min-height: 550px;
+  max-height: 550px;
   background: #FFFFFF;
   text-align: center;
-  /* margin: auto; */
+  padding: 64px 24px;
 }
 
-.home-why-create h1 {
+.home-why-create-header {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 48px;
+}
+
+.home-why-create-header h1 {
   font-size: 24px;
   font-weight: 700;
   line-height: 115%;
@@ -48,13 +73,70 @@
   margin-bottom: 16px;
 }
 
-.home-why-create p {
+.home-why-create-header p {
   font-size: 16px;
   font-weight: 400;
   color: #000000;
   opacity: 0.6;
-  /* width: 100%; */
-  /* max-width: 600px; */
+  width: 100%;
+  max-width: 700px;
   letter-spacing: 1.2px;
+}
+
+.home-why-create-cards {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 24px;
+  width: 100%;
+  max-width: 1000px;
+  margin: 0 auto;
+}
+
+.home-why-create-card {
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
+  gap: 24px;
+  padding: 24px;
+  border: 1px solid #e6e6e6;
+  border-radius: 10px;
+}
+
+.home-why-create-card-header {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+}
+
+.home-why-card-icon {
+  padding: 10px;
+  border-radius: 5px;
+}
+
+.blue-bg .home-why-card-icon {
+  background: #DBE9FE;
+}
+.green-bg .home-why-card-icon {
+  background: #DCFCE7;
+}
+.purple-bg .home-why-card-icon {
+  background: #F3E8FF;
+}
+
+.home-why-create-card-header h2 {
+  font-size: 20px;
+  font-weight: 600;
+  color: #000000;
+  opacity: 0.6;
+}
+
+.home-why-create-card p {
+  font-size: 14px;
+  font-weight: 400;
+  color: #000000;
+  opacity: 0.6;
 }
 </style>
