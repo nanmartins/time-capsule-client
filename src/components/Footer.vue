94e7e474133c54f6f37c71b38d44262bbd752e96
Renan Martins <renanmartins989@gmail.com>
2025-08-16 23:18:41 +0100

    footer basic done, will finish the color pallete first before end all the animations

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,23 +6,24 @@
       <h1>Timenest</h1>
     </div>
 
-    <p>Your bridge to the future, one message at a time</p>
+    <p class="footer-description">Your bridge to the future, one message at a time</p>
 
     <span>Connect with us</span>
 
     <div class="footer-socials-container">
-      <LinkedinSVG :width="'28'" :height="'28'" />
-      <FacebookSVG :width="'28'" :height="'28'" />
-      <InstagramSVG :width="'34'" :height="'34'" />
-      <GithubSVG :width="'28'" :height="'30'" />
+      <LinkedinSVG :width="'28'" :height="'28'" :stroke="'#FFFFFF'" :stroke-width="1" />
+      <FacebookSVG :width="'28'" :height="'28'" :stroke="'#FFFFFF'" :stroke-width="1" />
+      <InstagramSVG :width="'34'" :height="'34'" :stroke="'#FFFFFF'" :stroke-width="7" />
+      <GithubSVG :width="'28'" :height="'30'" :stroke="'#FFFFFF'" :stroke-width="9" />
     </div>
 
-    <div>
+    <div class="footer-terms-container">
       <p>Terms of Use</p>
+      <p>|</p>
       <p>Privacy Policy</p>
     </div>
 
-    <p>&copy; {{ new Date().getFullYear() }} Timenest. Made with ❤️ for your future self.</p>
+    <p class="footer-copyright">&copy; {{ new Date().getFullYear() }} Timenest. Made with ❤️ for your future self.</p>
   </footer>
 </template>
 
@@ -44,40 +45,82 @@ import GithubSVG from '@/assets/icons/GithubSVG.vue'
   align-items: center;
   text-align: center;
   gap: 10px;
-  padding: 24px;
-  background-color: #FAF9F6;
-  border-top: 1px solid #e6e6e6;
+  padding: 48px 24px;
+  background-color: #001925;
 }
 
 .footer-logo-container {
   display: flex;
   gap: 10px;
   align-items: center;
-  opacity: 0.7;
+  opacity: 0.8;
 }
 
 .footer-logo-container img {
-  width: 60px;
-  height: 60px;
+  width: 35px;
+  height: 35px;
   margin-bottom: 5px;
+  filter: invert(1);
 }
 
 .footer-logo-container h1 {
-  font-size: 64px;
+  font-size: 34px;
   font-weight: 400;
-  color: #000000;
+  color: #ffffff;
   letter-spacing: 10px;
 }
 
+.footer-description {
+  font-size: 18px;
+  font-weight: 400;
+  color: #ffffff;
+  opacity: 0.6;
+  letter-spacing: 1.5px;
+  margin-bottom: 30px;
+}
 
-
-
-
+.footer-container span {
+  font-size: 14px;
+  font-weight: 500;
+  color: #ffffff;
+  opacity: 0.6;
+  width: 100%;
+  letter-spacing: 1.2px;
+}
 
 .footer-socials-container {
   display: flex;
   justify-content: center;
   align-items: center;
-  gap: 20px;
+  gap: 30px;
+  opacity: 0.7;
 }
+
+.footer-terms-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  margin: 10px 0;
+  flex-wrap: nowrap;
+}
+
+.footer-terms-container p {
+  font-size: 14px;
+  font-weight: 500;
+  color: #ffffff;
+  opacity: 0.6;
+  letter-spacing: 1.2px;
+  white-space: nowrap;
+  padding: 20px 0;
+}
+
+.footer-copyright {
+  font-size: 14px;
+  font-weight: 500;
+  color: #ffffff;
+  opacity: 0.5;
+  letter-spacing: 1.2px;
+}
+
 </style>
